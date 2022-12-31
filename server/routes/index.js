ae4ca31d1f41329526c75b265206edd369bb379c
Adi142857 <aditya14iyer@gmail.com>
2022-12-31 14:44:22 +0530

    router

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,8 @@ const feature = require('./feature')
 
 router.use('/auth', auth);
 router.use('/', feature)
+router.get('/', (req, res) => {
+    res.json({ message: 'Api is working' });
+})
 
 module.exports = router;
